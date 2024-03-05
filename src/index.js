bef38cadf6445bd6f9c360cb02824128916810a4
Nakod55 <nakodvenkatesh43@gmail.com>
2024-03-05 09:27:52 +0530

    submit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const fs = require('fs').promises;
-const fileName = 'myfile.txt';
+const fileName = '../myfile.txt';
 
 const updateFile = async (fileName, fileContent) => {
     //Write your code here to overwrite the file content
     //Don't change function name
-    
+    await fs.writeFile(fileName,fileContent);
 };
+updateFile(fileName,"Newton School, is an online learning platform.");
 
 
 module.exports = updateFile;
